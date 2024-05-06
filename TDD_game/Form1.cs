1f53ed9f4ba5f2629c7e43c96a8eb498d0d5d3cd
sonya <sophie.zemskaya.altsu@yandex.ru>
2024-05-06 09:37:39 +0700

    Create start form representation

--- TDD_game/Form1.cs
+++ TDD_game/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +13,40 @@ namespace TDD_game
 {
     public partial class Form1 : Form
     {
+        private string songPath;
+        private int number;
+        private SoundPlayer player;
         public Form1()
         {
             InitializeComponent();
+            gamerBox1.Visible = false;
+            gamerBox2.Visible = false;
+            gamerBox3.Visible = false;
+            song1.Visible = false;
+            song2.Visible = false;
+            song3.Visible = false;
+            song4.Visible = false;
+            song5.Visible = false;
+            song6.Visible = false;
+            song7.Visible = false;
+            song8.Visible = false;
+            song9.Visible = false;
+            song10.Visible = false;
+            song11.Visible = false;
+            song12.Visible = false;
+            song13.Visible = false;
+            song14.Visible = false;
+            song15.Visible = false;
+            song16.Visible = false;
+            song17.Visible = false;
+            song18.Visible = false;
+            song19.Visible = false;
+            song20.Visible = false;
+            range1.Enabled = false;
+            range2.Enabled = false;
+            range3.Enabled = false;
         }
+        
 
     }
 }
