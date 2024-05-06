b1828f5d3b089028d8b43baf4f37cceaf0e13818
sonya <sophie.zemskaya.altsu@yandex.ru>
2024-05-06 09:40:15 +0700

    Add system.windows.form.dll, create link to tdd_game, create first test

--- TDD_test/UnitTest1.cs
+++ TDD_test/UnitTest1.cs
@@ -1,15 +1,26 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using System.Windows.Forms;
+using TDD_game;
 
 namespace TDD_test
 {
     [TestClass]
-    public class UnitTest1
+    public class AddGamerButtonTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddGamerTest()
         {
+            Form1 form1 = new Form1();
+            // Arrange
+            var gb = new GroupBox();
+            var tb = new TextBox();
 
+            // Act
+            form1.AddGamer(gb, tb);
+
+            // Assert
+            Assert.IsTrue(gb.Visible);
+            Assert.IsTrue(tb.Enabled);
         }
     }
 }
